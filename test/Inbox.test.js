4a20017868143883253f115c87102bd48a29dc00
mayocca <5626046+mayocca@users.noreply.github.com>
2021-12-12 04:21:09 -0300

    Created message tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -7,13 +7,14 @@ const web3 = new Web3(ganache.provider());
 const { interface, bytecode } = require('../compile');
 let accounts;
 let inbox;
+const INITIAL_MESSAGE = 'Hello world!';
 
 beforeEach(async () => {
   
   accounts = await web3.eth.getAccounts();
 
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hello world!'] })
+  .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
     .send({ from: accounts[0], gas: '1000000' });
   
 });
@@ -22,6 +23,18 @@ describe('Inbox', () => {
 
   it('deploys a contract', () => {
     assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    let message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_MESSAGE);
+  });
+
+  it('can change the message', async () => {
+    let newMessage = '¡Hola mundo!';
+    await inbox.methods.setMessage(newMessage).send({ from: accounts[0] });
+    let actualMessage = await inbox.methods.message().call();
+    assert.equal(actualMessage, newMessage);
   });
 
 });
