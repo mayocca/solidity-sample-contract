05086f9da04fd674ab02ea2213d3eb97d2ddc389
mayocca <5626046+mayocca@users.noreply.github.com>
2021-12-12 02:10:36 -0300

    Added contract template

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,4 +1,14 @@
 pragma solidity ^0.4.17;
-// linter warnings (red underline) about pragma version can igonored!
 
-// contract code will go here
+contract Inbox {
+    string public message;
+
+    function Inbox(string _initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage(string _message) public {
+        message = _message;
+    }
+
+}
