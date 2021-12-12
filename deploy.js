3d06a391d008ad7542040e51079e63f8146af1d9
mayocca <5626046+mayocca@users.noreply.github.com>
2021-12-12 20:35:07 -0300

    Created deployment script

--- deploy.js
+++ deploy.js
@@ -1,1 +1,32 @@
-// deploy code will go here
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const HDWalletProvicer = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+
+const provider = new HDWalletProvicer(
+  process.env.PRIVATE_KEY,
+  'wss://rinkeby.infura.io/ws/v3/' + process.env.INFURA_PID
+);
+
+const web3 = new Web3(provider);
+
+
+const deploy = async () => {
+  
+  const accounts = await web3.eth.getAccounts();
+
+  console.log(`Attempting to deploy from account ${accounts[0]}`);
+
+  const tx = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hello world!'] })
+    .send({ from: accounts[0], gas: '1000000' });
+
+  console.log(`Contract deployed to ${tx.options.address}`)
+
+};
+
+deploy();
